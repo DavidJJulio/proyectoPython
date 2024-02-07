38e7708c5ca24bceb629780c33b8f8d0f4978725
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-02-07 11:49:53 -0500

    build: :construction: Progresando, pero ta complicao

--- Modulos/CRUDC.py
+++ Modulos/CRUDC.py
@@ -1,5 +1,6 @@
 import os
 import json
+from Modulos import funciones
 os.system('clear')
 Campers = ("""
 
@@ -21,85 +22,19 @@ def sistemac():
         opc = int(input("¿A que opcion desea ingresar?: "))
         match(opc):
          case 1: 
-          crearcamper()
-         case 2:
-          buscarcamper()
-         case 3:
-          actualizarcamper()
-         case 4:
-          eliminarcamper()
-         case 5: 
-          bandera = False
+          funciones.crearcamper()
+         # case 2:
+         #  buscarcamper()
+         # case 3:
+         #  actualizarcamper()
+         # case 4:
+         #  eliminarcamper()
+         # case 5: 
+         #  bandera = False
 
 
 
-def crearcamper():
-    with open('JsonCampers.json', 'r') as f:
-        Lista = json.loads(f.read())
-        x = len(Lista)
-        os.system('clear')
-        Lista.append({
-           "Nombre" : input("Ingrese el nombre del Camper: "),
-           "Apellido" : input("Ingrese el apellido del Camper: "),
-           "Edad" : int(input("Ingrese la edad del Camper: ")),
-           "Genero" : input("Ingrese el genero del Camper: "),
-           "Numero" : [
-              {
-                 f"{'fijo' if(int(input('1.Fijo  0.Celular: ')))else 'Celular'}":
-                    input(f'Numero de contacto {x+1}: ')
-              }
-              for x in range((int(input("¿Cuantos numeros de contacto tiene?: "))))
-           ],
-           "Estado" : "",
-           "id" : input("Ingrese el numero de identidad del Camper: ")
-        })
-        if Lista[x-1]["Edad"] < 18:
-           Lista[x].update({
-              "Acudiente" : input("Ingrese el nombre de su acudiente: "),
-              "CelAcudiente" : input("Ingrese el numero telefonico de el acudiente: ")
-              })
 
-    with open('JsonCampers.json', 'w') as file:
-        json.dump(Lista, file, indent = 4)
-        file.close()
         
-def buscarcamper():
-        with open('JsonCampers.json', 'r') as f:
-           Lista = json.loads(f.read())
-           x = len(Lista)
-           f.close()
 
-
-        for i, item in enumerate(Lista):
-           print(f"""
---------------
-{Lista[i]["Nombre"]} {Lista[i]["Apellido"]}
-{Lista[i]["Edad"]} Años, id {Lista[i]["id"]}
-Genero {Lista[i]["Genero"]} 
-Numero {Lista[i]["Numero"]}
---------------
-
-""")
-           i+=1
-
-
-        busqueda = (input("Ingrese el id del estudiante: "))
-        if busqueda == Lista[x-1]["id"]:
-           os.system('clear')
-           for i,val in Lista[x-1].items():
-              print(f"""
---------------
-{Lista[i]['Nombre']} {Lista[i]['Apellido']}
-{Lista[i]['Edad']} Años, id {Lista[i]['id']}
-Genero {Lista[i]['Genero']} 
-Numero {Lista[i]['Numero']}
---------------""")
-           x = input("")
-        else:
-           print("No existe este estudiante")
-
-def actualizarcamper():
-    return 0
-def eliminarcamper():
-    return 0
 
--- Modulos/CRUDT.py
+++ Modulos/CRUDT.py
@@ -1,5 +1,6 @@
 import os
 import json
+from Modulos import funciones 
 os.system('clear')
 Horarios = ("  ---- HORARIOS LABORALES ----\n1. ---- 6:00 AM - 10:00 AM ----\n2. ---- 10:00 AM - 1:00 PM ----\n3. ---- 2:00 PM - 6:00 PM  ----\n4. ---- 6:00 PM - 10:00 PM ----")
 Trainers = ("""
@@ -9,9 +10,8 @@ Trainers = ("""
 - - - - - - - - - - TRAINERS - - - - - - - - - --
 - - - - - - - - - - - - - - - - - - - - - - - - -
 - - - - - - - - - - - - - - - - - - - - - - - - -
-1. CREAR TRAINER              2. BUSCAR TRAINER
-3. ACTUALIZAR TRAINER         4. ELIMINAR TRAINER
-5. SALIR
+1. CREAR TRAINER              2. ELIMINAR TRAINER
+3. SALIR
 """)
 
 def sistemat():
@@ -19,120 +19,22 @@ def sistemat():
  while bandera:
         os.system('clear')
         print(Trainers)
-        opc = int(input("¿A que opcion desea ingresar?: "))
-        match(opc):
-         case 1: 
-          creartrainer()
-         case 2:
-          buscartrainer()
-         case 3:
-          actualizartrainer()
-         case 4:
-          eliminartrainer()
-         case 5: 
-          bandera = False
+        try:
+            opc = int(input("¿A que opcion desea ingresar?: "))
+            if(3 >= opc > 0):
+               match(opc):
+                  case 1: 
+                     funciones.creartrainer()
+                  case 2:
+                     funciones.eliminartrainer()
+                  # case 3:
+                     #  actualizartrainer()
+                  # case 4:
+                     #  eliminartrainer()
+                  # case 5: 
+                     #  bandera = False
+        except: 
+         print("Ingrese un numero no un caracter.")
+         x = input("Enter para continuar...")
+            
     
-    
-
-
-def creartrainer():
-    with open('JsonTrainers.json', 'r') as f:
-        Lista = json.loads(f.read())
-        x = len(Lista)
-        os.system('clear')
-        Lista.append({
-           "Nombre" : input("Ingrese el nombre del Trainer: "),
-           "Apellido" : input("Ingrese el apellido del Trainer: "),
-           "Edad" : int(input("Ingrese la edad del Trainer: ")),
-           "Genero" : input("Ingrese el genero del Trainer: "),
-           "Numero" : [
-              {
-                 f"{'fijo' if(int(input('1.Fijo  0.Celular: ')))else 'Celular'}":
-                    input(f'Numero de contacto {x+1}: ')
-              }
-              for x in range(int(input("¿Cuantos numeros de contacto tiene?: ")))
-           ],
-           "Estado" : "",
-           "id" : int(input("Ingrese el numero de identidad del Trainer: ")),
-           "Horario" : [    
-                            input("\n\n\n\n  ---- HORARIOS LABORALES ----\n1. ---- 6:00 AM - 10:00 AM ----\n2. ---- 10:00 AM - 1:00 PM ----\n3. ---- 2:00 PM - 6:00 PM  ----\n4. ---- 6:00 PM - 10:00 PM ----\n\n  Seleccione un horario: ") 
-                            for i in range(int(input("Cuantas jornadas trabajará el trainer? (1 Jornada = 4 horas): ")))
-           ]
-        })
-
-
-    with open('JsonTrainers.json', 'w') as file:
-        json.dump(Lista, file, indent = 4)
-        file.close()
-def buscartrainer():
-           with open('JsonTrainers.json', 'r') as f:
-            Lista = json.loads(f.read())
-            x = len(Lista)
-            f.close()
-
-
-           for i, item in enumerate(Lista):
-            print(f"""
---------------
-{Lista[i]["Nombre"]} {Lista[i]["Apellido"]}
-{Lista[i]["Edad"]} Años, id {Lista[i]["id"]}
-Genero {Lista[i]["Genero"]} 
-Numero {Lista[i]["Numero"]}
---------------
-
-""")
-           i+=1
-
-
-           busqueda = (input("Ingrese el id del trainer: "))
-           if busqueda == Lista[x-1]["id"]:
-            os.system('clear')
-            for i,val in Lista[x-1].items():
-              print(f"""
---------------
-{Lista[i]["Nombre"]} {Lista[i]["Apellido"]}
-{Lista[i]["Edad"]} Años, id {Lista[i]["id"]}
-Genero {Lista[i]["Genero"]} 
-Numero {Lista[i]["Numero"]}
---------------""")
-              x = input("")
-           else:
-            print("No existe este trainer")
-            x = input("")
-    
-def actualizartrainer():
-           with open('JsonTrainers.json', 'r') as f:
-            Lista = json.loads(f.read())
-            x = len(Lista)
-            f.close()
-
-
-           for i, item in enumerate(Lista):
-            print(f"""
---------------
-{Lista[i]["Nombre"]} {Lista[i]["Apellido"]}
-{Lista[i]["Edad"]} Años, id {Lista[i]["id"]}
-Genero {Lista[i]["Genero"]} 
-Numero {Lista[i]["Numero"]}
---------------
-
-""")
-           i+=1
-
-
-           busqueda = int((input("Ingrese el id del trainer: ")))
-           if busqueda == Lista[x-1]["id"]:
-            os.system('clear')
-            for i,val in Lista[x-1].items():
-              print(f"""
---------------
-{Lista[i]["Nombre"]} {Lista[i]["Apellido"]}
-{Lista[i]["Edad"]} Años, id {Lista[i]["id"]}
-Genero {Lista[i]["Genero"]} 
-Numero {Lista[i]["Numero"]}
---------------""")
-              x = input("")
-           else:
-            print("No existe este trainer")
-def eliminartrainer():
-    return 0
